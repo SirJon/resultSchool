0a75d9aaf61e8de7600f435a1a827e1fb698cb1b
Silin Evgenii <e.silin@avm-t.ru>
2023-06-07 12:49:58 +0300

    test3

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,82 @@
-//start 11:24
+//start 11:45
 
-//Создаю интерфейс для параметров функции
-const PERCENT: number = 100;
-const CREDIT_RATIO: number = 1.2;
-const INSTALLMENT_FACTOR: number = 1;
-interface totalPriceProps {
-  price: number
-  discount: number
-  isInstallment: boolean
-  months: number
+interface IPost {
+  id: string
+  title: string
+  body: string
 }
 
-const totalPrice = ({ price, discount, isInstallment, months }: totalPriceProps): number => {   //возвращаемое значение - number
-  //возвращаем стоимость с учетом скидки и рассрочки
-  return (price * ((PERCENT - discount) / PERCENT)) / months * (isInstallment ? INSTALLMENT_FACTOR : CREDIT_RATIO)
+interface IByid {
+  [id: string]: IPost
+}
+interface INormalize {
+  byId: IByid
+  allIds: string[]
+}
+type Posts = IPost[];
+
+const posts = [
+  {
+    id: '62e69d5a5458aac0ed320b35',
+    title: 'id labore ex et quam laborum',
+    body: 'laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibusdolor quam autem quasireiciendis et nam sapiente accusantium'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b1c',
+    title: 'quo vero reiciendis velit similique earum',
+    body: 'est natus enim nihil est dolore omnis voluptatem numquamet omnis occaecati quod ullam at voluptatem error expedita pariaturnihil sint nostrum voluptatem reiciendis et'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b32',
+    title: 'odio adipisci rerum aut animi',
+    body: 'quia molestiae reprehenderit quasi aspernaturaut expedita occaecati aliquam eveniet laudantiumomnis quibusdam delectus saepe quia accusamus maiores nam estcum et ducimus et vero voluptates excepturi deleniti ratione'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b39',
+    title: 'alias odio sit',
+    body: 'non et atqueoccaecati deserunt quas accusantium unde odit nobis qui voluptatemquia voluptas consequuntur itaque doloret qui rerum deleniti ut occaecati'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b53',
+    title: 'vero eaque aliquid doloribus et culpa',
+    body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b19',
+    title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
+    body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in'
+  },
+  {
+    id: '62e69d5a5458aac0ed320b25',
+    title: 'repellat consequatur praesentium vel minus molestias voluptatum',
+    body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor'
+  }
+];
+
+const normalizeData = (unnormalizedData: Posts): INormalize => {
+  const resilt: INormalize = {
+    byId: {},
+    allIds: [],
+  }
+  unnormalizedData.forEach(it => {
+    resilt.byId[it.id] = {...it};
+    resilt.allIds.push(it.id)
+  })
+  return resilt
 };
 
-console.log(totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 }))
-console.log(totalPrice({ price: 100000, discount: 25, isInstallment: false, months: 12 }))
+console.log(normalizeData(posts));
+/**
+ * {
+ *    byId: {
+ *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
+ *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
+ *      ...
+ *    },
+ *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
+ * }
+ */
 
-//end 11:34
+//end 12:48
+
+//ВРЕМЯ ВЫПОЛНЕНИЯ - 1 час
